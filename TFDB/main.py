7ae2ceca7403bff19a21cdca28b70b42c32d311a
CarolYeh910 <Carol-910@outlook.com>
2021-10-21 21:14:59 +0800

    Modify Port

--- TFDB/main.py
+++ TFDB/main.py
@@ -4,4 +4,4 @@ from api import app
 
 if __name__ == '__main__':
    app.config['JSON_AS_ASCII'] = False
-   app.run(host='127.0.0.1', port='5122', debug=True)
+   app.run(host='0.0.0.0', port='5122', debug=True)
